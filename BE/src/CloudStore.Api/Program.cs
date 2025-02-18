3531bc4df4576b93fcaaa3209c4f6da6b77f80ea
Nikola <knez.nikola00@outlook.com>
2025-02-18 23:10:21 +0100

    update program.cs

--- BE/src/CloudStore.Api/Program.cs
+++ BE/src/CloudStore.Api/Program.cs
@@ -1,49 +1,17 @@
-using System.Text;
 using CloudStore.Application;
 using CloudStore.Infrastructure;
-using CloudStore.Infrastructure.Authentication;
-using CloudStore.Infrastructure.Storage;
 using CloudStore.Persistence;
 using CloudStore.Presentation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
-var services = builder.Services;
+
 var configuration = builder.Configuration;
 
-// Dependency Injection
-services.AddPersistence(configuration);
-services.AddInfrastructure();
-services.AddApplication();
-services.AddPresentation();
-
-// Options configuration
-services.Configure<StorageBucketOptions>(
-    builder.Configuration.GetSection("StorageBucket"));
-
-var jwtSection = configuration.GetSection("Jwt");
-services.Configure<JwtOptions>(jwtSection);
-
-var secretKey = jwtSection["SecretKey"];
-var issuer = jwtSection["Issuer"];
-var audience = jwtSection["Audience"];
-
-services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = issuer,
-            ValidAudience = audience,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(secretKey!))
-        };
-    });
+builder.Services
+    .AddApplication()
+    .AddPresentation()
+    .AddInfrastructure(configuration)
+    .AddPersistence(configuration);
 
 var app = builder.Build();
 
@@ -54,8 +22,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
--- BE/src/CloudStore.Infrastructure/DependencyInjection.cs
+++ BE/src/CloudStore.Infrastructure/DependencyInjection.cs
@@ -1,19 +1,46 @@
+using System.Text;
 using CloudStore.Application.Abstractions;
 using CloudStore.Infrastructure.Authentication;
 using CloudStore.Infrastructure.Storage;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CloudStore.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IPasswordHasher, PasswordHasher>();
         services.AddScoped<ITokenGenerator, JwtProvider>();
         services.AddScoped<IStorageService, StorageService>();
         services.AddScoped<IFileFormatter, FileUrlFormatter>();
         services.AddScoped<IFileStorageService, FileStorageService>();
+
+        services.Configure<StorageBucketOptions>(
+            configuration.GetSection("StorageBucket"));
+
+        var jwtSection = configuration.GetSection("Jwt");
+
+        services.Configure<JwtOptions>(jwtSection);
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSection["Issuer"],
+                    ValidAudience = jwtSection["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSection["SecretKey"]!))
+                };
+            });
 
         return services;
     }
